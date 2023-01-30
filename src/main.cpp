b808e6cd08c8b55c25641ef96b49a114f53294cf
raphaelabrantes <antenabr2@gmail.com>
2023-01-31 00:01:11 +0800

    main: updated to test

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,25 @@
 
 #include <iostream>
 #include <fstream>
-#include <sstream>
+#include <cryptopp/sha.h>
+#include <cryptopp/hex.h>
+#include <boost/beast/core.hpp>
+#include <boost/asio/ip/tcp.hpp>
 #include "bencode/BencodeObject.h"
+#include "AnnounceSynchronizer.h"
+#include "PieceManager.h"
+
+
 
 
 int main() {
     std::ifstream metafile("tests/alice_in_wonderland_librivox_archive.torrent");
     std::stringstream buffer;
     buffer << metafile.rdbuf();
-    BencodeObject becodeObject = BencodeObject::from_string(buffer.str());
-    if(becodeObject.is_dict() && !becodeObject.as_dict().values().empty()){
-        auto dict = becodeObject.as_dict().values();
-        auto announce_url = dict.at("announce").as_string();
-    }
+    std::string metafile_contents = buffer.str();
+    BencodeObject becode_object = BencodeObject::from_string(metafile_contents);
+    PieceManager piece_manager(becode_object.as_dict().at("info").as_dict().at("files"));
+    AnnounceSynchronizer announce_synchronizer(becode_object);
+    announce_synchronizer.exec();
     return 0;
 }
