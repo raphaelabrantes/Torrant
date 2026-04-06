e7a1286dec2200078a4e2043a6cae783d3936ce0
Raphael Abrantes <raphael@abrantes.dev>
2026-04-06 21:59:44 +0800

    Server: mpv for piece acquirement

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,10 +6,9 @@
 #include <iostream>
 #include <boost/asio/connect.hpp>
 #include <boost/asio/write.hpp>
-#include <boost/asio/ip/tcp.hpp>
 
 
-std::string get_handshake(std::string hash_info, std::string host_id) {
+std::string Server::generate_handshake_package(const std::string& hash_info, const std::string& host_id) {
     std::string data;
     data.push_back(0x13);
     data += "BitTorrent protocol";
@@ -19,34 +18,62 @@ std::string get_handshake(std::string hash_info, std::string host_id) {
     return data;
 }
 
+char keep_alive[4]  = { 0, 0, 0, 0 };
+char choke [5] = {0, 0, 0, 1, 0};
+char unchoke [5] = {0, 0, 0, 1, 1};
+char interested [5] = {0, 0, 0, 1, 2};
+char not_interested [5] = {0, 0, 0, 1, 3};
+char have [5] = {0, 0, 0, 5, 4};
 
-void Server::exec() {
+
+boost::optional<connection> Server::create_connection(const std::string &handshake_package, const std::string &ip, u_int16_t port) {
     try {
-        auto [ip, port] = m_announce_synchronizer.get_peers().at(0);
-
-        boost::asio::ip::tcp::acceptor acceptor(m_io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 6881));
         boost::asio::ip::tcp::socket socket(m_io_context);
-        boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::make_address(ip), port);
+        const boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::make_address(ip), port);
         boost::asio::ip::tcp::resolver resolver(m_io_context);
         boost::asio::ip::tcp::resolver::results_type endpoints = resolver.resolve(endpoint);
 
-        boost::system::error_code error;
         boost::asio::connect(socket, endpoints);
+        socket.write_some(boost::asio::buffer(handshake_package));
 
-        auto handshake = get_handshake(m_announce_synchronizer.get_info_hash(), "-RP0001-EEJs5CQi7kyN");
-
-        socket.write_some(boost::asio::buffer(handshake), error);
-
-        std::array<char, 68> buf;
-        size_t len = socket.read_some(boost::asio::buffer(buf), error);
+        std::array<char, 68> buf{};
+        size_t len = socket.read_some(boost::asio::buffer(buf));
         std::string connected(buf.data(), len);
-        std::cout << "connected to: " << connected.substr(48) << std::endl;
+        const std::string client = connected.substr(48);
+        std::cout << "connected to: "  << std::endl;
+
+        return std::move(connection{&socket, client});
 
     }
-    catch (std::exception& e)
-    {
+    catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
+        return boost::none;
     }
+}
+
+
+void Server::start_file_distribution(connection &connection) {
+    // auto wanted_piece = m_piece_manager.get_wanted_piece();
+    // boost::optional<> piece = ask_piece(connection, wanted_piece);
+    // if (piece) {
+    //     save(piece);
+    //     m_piece_manager.set_piece_acquired(wanted_piece);
+    // } else {
+    //     m_piece_manager.return_piece_to_queue(piece);
+    // }
+
+}
+
+void Server::exec() {
+    const auto handshake = generate_handshake_package(m_announce_synchronizer.get_info_hash(), "-RP0001-EEJs5CQi7kyN");
+
+    for (const auto& [ip, port]: m_announce_synchronizer.get_peers()) {
+        auto connection = create_connection(handshake, ip, port);
+        if (connection.is_initialized()) {
+            start_file_distribution(*connection);
+        }
+    }
+
 
 }
 
--- src/Server.h
+++ src/Server.h
@@ -1,23 +1,49 @@
 // Copyright (c) Raphael Prandini Thome de Abrantes 2026
 
 #pragma once
+#include <unordered_set>
 #include <boost/asio/io_context.hpp>
+#include <boost/optional.hpp>
+#include <boost/asio/ip/tcp.hpp>
 
 #include "AnnounceSynchronizer.h"
 #include "PieceManager.h"
 
 
+struct connection {
+    boost::asio::ip::tcp::socket *socket;
+    std::string client;
+    bool isConnected;
+    connection(boost::asio::ip::tcp::socket *socket, const std::string &client) : socket(socket), client(client), isConnected(false) {};
+    bool operator==(const connection& other) const { return client == other.client; };
+};
 
 
+template<>
+struct std::hash<connection> {
+    std::size_t operator()(const connection &c) const noexcept {
+        return std::hash<std::string>()(c.client);
+    }
+};
 
 class Server {
 public:
-    Server() = default;
-    Server(const PieceManager & piece_manager, const AnnounceSynchronizer & announce_synchronizer): m_piece_manager(piece_manager), m_announce_synchronizer(announce_synchronizer) {};
+    Server() = delete;
+    Server(const PieceManager & piece_manager, const AnnounceSynchronizer & announce_synchronizer):
+        m_piece_manager(piece_manager),
+        m_announce_synchronizer(announce_synchronizer) {};
+
+    void start_file_distribution(connection &connection);
+
     void exec();
 private:
+    static std::string generate_handshake_package(const std::string &hash_info, const std::string &host_id);
+    boost::optional<connection> create_connection(const std::string & handshake, const std::string & ip, u_int16_t port);
+
     PieceManager m_piece_manager;
     AnnounceSynchronizer m_announce_synchronizer;
     boost::asio::io_context m_io_context;
+    std::unordered_set<connection> m_connections = std::unordered_set<connection>();
+
 };
 
